7e191f028d43557b49e34f93c94675be56e0b7c4
connorkearns <ckearns1@ithaca.edu>
2024-09-04 00:13:22 -0400

    rectangle calcArea, doubleSize and their tests.

--- src/main/java/edu/ithaca/dragon/shapes/Rectangle.java
+++ src/main/java/edu/ithaca/dragon/shapes/Rectangle.java
@@ -1,21 +1,30 @@
 package edu.ithaca.dragon.shapes;
 
 public class Rectangle {
+    private double length;
+    private double width;
     
 
     public Rectangle(double length, double width){
-        throw new RuntimeException("Not implemented yet");
+        this.length = length;
+        this.width = width;
+        //throw new RuntimeException("Not implemented yet");
     }
 
     public double calcArea(){
-       throw new RuntimeException("Not implemented yet");
+        double area = this.length * this.width;
+        return area;
+       // throw new RuntimeException("Not implemented yet");
     }
 
-    public void doubleSize(){
-        throw new RuntimeException("Not implemented yet");
+    public double doubleSize(){
+        double doubledArea = this.length * this.width * 2; // is this alloud? Im not sure if im supposed to use the "this.length" here
+        return doubledArea;
+        //throw new RuntimeException("Not implemented yet");
     }
 
     public double longestLineWithin(){
+
         throw new RuntimeException("Not implemented yet");
     }
     
